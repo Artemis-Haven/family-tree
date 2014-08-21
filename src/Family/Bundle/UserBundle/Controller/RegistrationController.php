2fbd9f4f8942eaa71a8edfcf4667bcf9f5464463
Artemis-Haven <artemis.hactivation@gmail.com>
2014-08-22 00:33:00 +0200

    Ajout des templates de login et de register

---
+++ src/Family/Bundle/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Family\Bundle\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+class RegistrationController extends BaseController
+{
+    /*public function registerAction()
+    {
+        $response = parent::registerAction();
+
+        // do custom stuff
+
+        return $response;
+    }*/
+}
+
